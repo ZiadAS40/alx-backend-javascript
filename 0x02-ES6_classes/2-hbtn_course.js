f05867015b25369bcc77fae83e0b2c072bf050ae
ZiadAS40 <zead4036@gmail.com>
2024-10-04 03:55:19 +0300

    fix number 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   set name(name) {
@@ -18,7 +18,7 @@ class HolbertonCourse {
   }
 
   set length(length) {
-    if (typeof length !== 'number') {
+    if (typeof (length) !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = length;
