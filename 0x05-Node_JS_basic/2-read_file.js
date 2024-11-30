14bd263af8301ac0b99332fd0805b1a1f63f6e55
ZiadAS40 <zead4036@gmail.com>
2024-11-30 17:51:30 +0200

    re-adding the task number 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,22 +1,29 @@
 const fs = require('fs');
+const csv = require('csv-parser');
 
-function countStudents (path) {
-  try {
-    let data = fs.readFileSync(path, 'utf8').toString().split('\n');
-    data = data.slice(1, data.length - 1);
-    console.log(`Number of students: ${data.length}`);
-    const obj = {};
-    data.forEach((el) => {
-      const student = el.split(',');
-      if (!obj[student[3]]) obj[student[3]] = [];
-      obj[student[3]].push(student[0]);
-    });
-    for (const cls in obj) {
-      if (cls) console.log(`Number of students in ${cls}: ${obj[cls].length}. List: ${obj[cls].join(', ')}`);
-    }
-  } catch (err) {
+const students = [];
+const cs = [];
+const swe = [];
+
+const countStudents = (file) => {
+  if (!fs.existsSync(file)) {
     throw new Error('Cannot load the database');
   }
-}
+  fs.createReadStream(file)
+    .pipe(csv())
+    .on('data', (row) => {
+      students.push(row);
+      if (row.field === 'CS') {
+        cs.push(row.firstname);
+      } else {
+        swe.push(row.firstname);
+      }
+    })
+    .on('end', () => {
+      console.log(`Number of students: ${students.length}`);
+      console.log(`Number of students in CS: ${cs.length}. List: ${cs}`);
+      console.log(`Number of students in SWE: ${swe.length}. List: ${swe}`);
+    });
+};
 
 module.exports = countStudents;
