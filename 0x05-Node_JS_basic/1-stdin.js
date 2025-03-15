3121b7c2c20f35a8ae87c1f99def2b247abba539
ZiadAS40 <ziad403612@gmail.com>
2025-03-15 13:46:07 +0200

    fix file number 1 issue

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,9 @@
-console.log('Welcome to ALX, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
-    const chunk = process.stdin.read();
-  
-    if (chunk) {
-      process.stdout.write(`Your name is: ${chunk}`);
-    }
-  });
-
-process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
+process.stdin.on('end', () => {
+ process.stdout.write('This important software is now closing\n');
+});
