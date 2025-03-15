27685bc9b18d2bf38fc713053228b28099f7082f
ZiadAS40 <ziad403612@gmail.com>
2025-03-15 13:34:52 +0200

    fix file number 1 issue

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,5 +1,5 @@
 process.stdin.on('data', (data) => {
-  process.stdout.write(`your nameYour name is: ${data}`);
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
