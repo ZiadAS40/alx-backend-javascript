f5e943e2ed158e3e71af8b53e9f804942c0931a9
ZiadAS40 <ziad403612@gmail.com>
2025-03-15 13:55:29 +0200

    debug filse number 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,29 +1,28 @@
 const fs = require('fs');
 
 module.exports = function countStudents(path) {
-  const fileContent = fs.readFileSync(path, 'utf-8');
+  try {
+    const fileContent = fs.readFileSync(path, 'utf-8');
+    const lines = fileContent.split('\n');
+    lines.shift();
+    const fields = {};
 
-  if (!fileContent) {
+    for (const line of lines) {
+      const parsedLine = line.split(',');
+      const lineField = parsedLine[parsedLine.length - 1];
+      const firstName = parsedLine[0];
+
+      if (!(Array.isArray(fields[lineField]))) fields[lineField] = [];
+
+      fields[lineField].push(firstName);
+    }
+
+    process.stdout.write(`Number of students: ${lines.length}\n`);
+
+    for (const [key, value] of Object.entries(fields)) {
+      process.stdout.write(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}\n`);
+    }
+  } catch (e) {
     throw new Error('Cannot load the database');
-  }
-
-  const lines = fileContent.split('\n');
-  lines.shift();
-  const fields = {};
-
-  for (const line of lines) {
-    const parsedLine = line.split(',');
-    const lineField = parsedLine[parsedLine.length - 1];
-    const firstName = parsedLine[0];
-
-    if (!(Array.isArray(fields[lineField]))) fields[lineField] = [];
-
-    fields[lineField].push(firstName);
-  }
-
-  console.log(`Number of students: ${lines.length}`);
-
-  for (const [key, value] of Object.entries(fields)) {
-    console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
   }
 };
